285c7fde2bc12c695a5de3f20ac1c3e848a25c0b
smanghise <35621921+smanghise@users.noreply.github.com>
2018-01-20 18:42:31 -0800

    Update puzzle2.js

--- puzzle2.js
+++ puzzle2.js
@@ -1,20 +1,13 @@
-function downSixTimes {
-down
-down
-down
-down
-down
-down
+function move(steps,direction) {
+  var count = 0
+  while (count<steps) {
+    direction
+   count = count + 1
+  }
 }
 
-downSixTimes
-right 
-right
-right
-up
-up
-right
-right
-down
-down
-down
+move(6,down)
+move(3,right)
+move(2,up)
+move(2,right)
+move(3,down)
