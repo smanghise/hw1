b00b76f43059eab8346536bbaed025501d4b09a7
smanghise <35621921+smanghise@users.noreply.github.com>
2018-01-20 18:03:11 -0800

    Create puzzle9.js

---
+++ puzzle9.js
@@ -0,0 +1,13 @@
+function move(steps,direction) {
+  var count = 0
+  while (count<steps) {
+    direction
+    setColor("red")
+   count = count + 1
+  }
+}
+
+move(2,down)
+move(3,right)
+move(2,up)
+move(3,left)
