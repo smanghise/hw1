e6b03ccd640c9fc2e0ffe6148f92ac583741e25e
smanghise <35621921+smanghise@users.noreply.github.com>
2018-01-20 17:51:46 -0800

    Update puzzle6.js

--- puzzle6.js
+++ puzzle6.js
@@ -3,14 +3,26 @@ if (getColor == "red") {
   up
   up
   up
-} else {
-  down
-  down
-  down
+  if (getColor == "red") {
+    left
+    up
+  }
+ else {
+    right
+    up
+}   
 }
-if (getColor == "red") {
-  left
-  up
-} else {
+
+else {
+  down
+  down
+  down
+  if (getColor == "red") {
+    left
+    down
+  }       
+ else {
   right
-  up
+  down
+ }
+}
