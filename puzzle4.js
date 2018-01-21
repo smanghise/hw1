cf5e4becc67a405576b16e7c13342d20a69500c0
smanghise <35621921+smanghise@users.noreply.github.com>
2018-01-20 16:37:48 -0800

    Create puzzle4.js

---
+++ puzzle4.js
@@ -0,0 +1,17 @@
+down
+down
+down
+down
+right
+var firstColorSeen = getColor
+down
+setColor(firstColorSeen)
+right
+right
+up
+right
+var secondColorSeen = getColor
+down
+setColor(secondColorSeen)
+right
+right
