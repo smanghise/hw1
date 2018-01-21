b34948d03f27b1df5154b14377868d52d0f025c3
smanghise <35621921+smanghise@users.noreply.github.com>
2018-01-20 17:22:08 -0800

    Create puzzle8.js

---
+++ puzzle8.js
@@ -0,0 +1,39 @@
+function downEightTimes {
+  var steps = 0
+  while (steps < 8) {
+   down
+    steps = steps + 1
+  }
+}
+
+function downSevenTimes {
+  var steps = 0
+  while (steps < 7) {
+   down
+    steps = steps + 1
+  }
+}
+
+function upSevenTimes {
+  var steps = 0
+  while (steps < 7) {
+   up
+    steps = steps + 1
+  }
+}
+
+function rightTwoTimes {
+  right
+  right
+}
+
+downEightTimes
+rightTwoTimes
+upSevenTimes
+rightTwoTimes
+downSevenTimes
+rightTwoTimes
+upSevenTimes
+rightTwoTimes
+downSevenTimes
+right
