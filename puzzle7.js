77571583e41f81797a9f81d888cbaf62e0247978
smanghise <35621921+smanghise@users.noreply.github.com>
2018-01-20 17:10:43 -0800

    Create puzzle7.js

---
+++ puzzle7.js
@@ -0,0 +1,27 @@
+function rightTwoTimes {
+  right
+  right
+}
+
+function DownIfNotRed {
+  while (getColor != "red") {
+    down
+  }
+}
+
+function UpIfNotRed {
+  while (getColor != "red") {
+    up
+  }
+}
+
+DownIfNotRed
+rightTwoTimes
+UpIfNotRed
+rightTwoTimes
+DownIfNotRed
+rightTwoTimes
+UpIfNotRed
+rightTwoTimes
+DownIfNotRed
+right
